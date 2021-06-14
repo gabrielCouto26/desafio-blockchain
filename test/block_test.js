29694e6d48c9eaab241cbb836f4cbef46078c354
Gabriel Couto <gabrielcouto26@gmail.com>
2021-06-14 14:23:12 -0300

    instancia new Block no before

--- test/block_test.js
+++ test/block_test.js
@@ -2,30 +2,29 @@ const Block = require('../src/Block')
 const SHA256 = require('crypto-js/sha256')
 const { expect } = require('chai')
 
+let block;
+
 describe('Block', function(){
+    before(function(){
+        block = new Block("13/06/2021", 'Primeiro Bloco')
+    })
 
     context('calculateHash', function(){
         
         it('expect criptography to work', function(){
-            let block = new Block("13/06/2021", 'Primeiro Bloco')
-            let hash = block._calculateHash()
-
             let previousHash = ''
             let nonce = 0
             let mockHash = SHA256(previousHash + nonce + JSON.stringify('Primeiro Bloco') + "13/06/2021").toString()
             
-            expect(mockHash).to.be.eq(hash);
+            expect(mockHash).to.be.eq(block.hash);
         })
 
         it('expect criptography not to work', function(){
-            let block = new Block("13/06/2021", 'Primeiro Bloco')
-            let hash = block._calculateHash()
-
             let previousHash = ''
             let nonce = 0
             let mockHash = SHA256(previousHash + nonce + JSON.stringify('Segundo Bloco') + "13/06/2021").toString()
             
-            expect(mockHash).not.to.be.eq(hash);
+            expect(mockHash).not.to.be.eq(block.hash);
         })
 
     })
@@ -43,11 +42,9 @@ describe('Block', function(){
                 hash = SHA256(previousHash + nonce + JSON.stringify('Primeiro Bloco') + "13/06/2021").toString()
             }
 
-            let block = new Block("13/06/2021", 'Primeiro Bloco')
             block.mineBlock(difficulty)
 
             expect(hash).to.be.eq(block.hash)
-
         })
 
         it('expect mining not to work', function(){
@@ -62,11 +59,9 @@ describe('Block', function(){
                 hash = SHA256(previousHash + nonce + JSON.stringify('Primeiro Bloco') + "13/06/2021").toString()
             }
     
-            let block = new Block("13/06/2021", 'Primeiro Bloco')
             block.mineBlock(wrongDifficulty)
     
             expect(hash).not.to.be.eq(block.hash)
-    
         })
 
     })
