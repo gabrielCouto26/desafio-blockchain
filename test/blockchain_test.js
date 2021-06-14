6f46d8c0974e66d2a4e1fcea2fe22a8e89865502
Gabriel Couto <gabrielcouto26@gmail.com>
2021-06-14 00:21:04 -0300

    testando blockchain

---
+++ test/blockchain_test.js
@@ -0,0 +1,105 @@
+const Blockchain = require('../src/Blockchain')
+const Block = require('../src/Block')
+const { expect } = require('chai')
+
+describe('Blockchain', function(){
+
+
+    context('addBlock', function(){
+
+        it('expect to add first block', function(){
+            let blockchain = new Blockchain()
+            let block = new Block("13/06/2021", 'Primeiro Bloco')
+            
+            blockchain.addBlock(block)
+
+            expect(blockchain.chain).to.have.members([block])
+
+        })
+
+        it('expect to add more blocks', function(){
+            let blockchain = new Blockchain()
+            let block = new Block("13/06/2021", 'Primeiro Bloco')
+            let block2 = new Block("14/06/2021", 'Segundo Bloco')
+            
+            blockchain.addBlock(block)
+            blockchain.addBlock(block2)
+
+            expect(blockchain.chain).to.have.members([block, block2])
+        })
+    })
+
+    context('getLatestBlock', function(){
+
+        it('expect to get latest block', function(){
+            let blockchain = new Blockchain()
+            let block = new Block("13/06/2021", 'Primeiro Bloco')
+            let block2 = new Block("14/06/2021", 'Segundo Bloco')
+            
+            blockchain.addBlock(block)
+            blockchain.addBlock(block2)
+
+            let latestBlock = blockchain.getLatestBlock()
+
+            expect(latestBlock).to.be.eq(block2)
+        })
+
+        it('expect not to get latest block', function(){
+            let blockchain = new Blockchain()
+            let block = new Block("13/06/2021", 'Primeiro Bloco')
+            let block2 = new Block("14/06/2021", 'Segundo Bloco')
+            
+            blockchain.addBlock(block)
+            blockchain.addBlock(block2)
+
+            let latestBlock = blockchain.getLatestBlock()
+
+            expect(latestBlock).not.to.be.eq(block)
+        })
+
+    })
+
+
+    context('isChainValid', function(){
+
+        it('expect chain to be valid', function(){
+            let blockchain = new Blockchain()
+            let block = new Block("13/06/2021", 'Primeiro Bloco')
+            let block2 = new Block("14/06/2021", 'Segundo Bloco')
+            
+            blockchain.addBlock(block)
+            blockchain.addBlock(block2)
+
+            let chainValida = true
+
+            for(let i = 1; i < blockchain.chain.length; i++){
+                const currentBlock = blockchain.chain[i]
+                const previousBlock = blockchain.chain[i - 1]
+    
+                if(currentBlock.hash !== currentBlock._calculateHash())
+                    chainValida = false // bloco foi alterado e sua hash mudou
+                
+                if(currentBlock.previousHash !== previousBlock.hash)
+                    chainValida = false // bloco nao aponta para bloco anterior
+            }
+
+            
+            expect(blockchain._isChainValid()).to.be.eq(chainValida)
+        })
+
+        it('expect chain not to be valid', function(){
+            let blockchain = new Blockchain()
+            let block = new Block("13/06/2021", 'Primeiro Bloco')
+            let block2 = new Block("14/06/2021", 'Segundo Bloco')
+
+            blockchain.addBlock(block)
+            blockchain.addBlock(block2)
+
+            block2.previousHash = ''
+
+            expect(blockchain._isChainValid()).not.to.be.eq(true)
+        })
+
+    })
+
+})
