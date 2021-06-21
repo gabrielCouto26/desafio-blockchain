4bcf7506d747cd02c4d1ae35a5825de44ace4e8e
Gabriel Couto <gabrielcouto26@gmail.com>
2021-06-20 21:33:00 -0300

    adiciona novo bloco

--- src/drawer.js
+++ src/drawer.js
@@ -4,6 +4,7 @@ const Drawer = {
         let block = document.createElement("div")
         block.classList.add("card")
         block.style.width = "18rem"
+        block.style.margin = "2rem 0"
 
         let blockBody = document.createElement("div")
         blockBody.classList.add("card-body")
@@ -16,6 +17,11 @@ const Drawer = {
         blockText.classList.add('card-text')
         blockText.textContent = data.toString()
 
+        if(parseInt(index, 10) > 1){
+            let arrow = this.drawArrow()
+            document.querySelector('.blockchain').appendChild(arrow)
+        }
+
         blockBody.appendChild(blockTitle)
         blockBody.appendChild(blockText)
         block.appendChild(blockBody)
@@ -23,12 +29,21 @@ const Drawer = {
     },
 
     drawBlockchain(blockchain){
+        document.querySelector('.blockchain').innerHTML = ""
         for(let block of blockchain.chain){
             this.drawBlock(block.index, block.data)
         }
+    },
+
+    drawArrow(){
+        let arrow = document.createElement('i')
+        arrow.classList.add('rotate')
+        arrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                            </svg>`
+        return arrow
     }
 
 }
-// Drawer.drawBlock("1", "teste")
 
 module.exports = Drawer
--- src/main.js
+++ src/main.js
@@ -5,12 +5,20 @@ const Drawer = require('./drawer')
 let coutoCoin = new Blockchain()
 let block1 = new Block("13/06/2021", 'Primeiro Bloco')
 let block2 = new Block("14/06/2021", 'Segundo Bloco')
-let block3 = new Block("15/06/2021", 'Terceiro Bloco')
 
 coutoCoin.addBlock(block1)
 coutoCoin.addBlock(block2)
-coutoCoin.addBlock(block3)
 
 // console.log(JSON.stringify(coutoCoin, null, 2))
 
 Drawer.drawBlockchain(coutoCoin)
+
+document.getElementById('btnAddBlock').addEventListener('click', function(e){
+    e.preventDefault()
+    let blockData = document.getElementById('blockData').value
+    let block = new Block("20/06/2021", blockData)
+
+    coutoCoin.addBlock(block)
+
+    Drawer.drawBlockchain(coutoCoin)
+})
